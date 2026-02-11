6db56da86ef4f277e69efc3e39031be6618ada1d
vitorpita <vitorhpita@gmail.com>
2026-02-11 20:38:11 -0300

    update

---
+++ umfg.venda.app/Models/PedidoModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace umfg.venda.app.Models
+{
+    public sealed class PedidoModel
+    {
+        private Guid _id = Guid.NewGuid();
+        private decimal _total = 0.0m;
+        private ObservableCollection<ProdutoModel> _produtos = [];
+
+        public Guid Id { get => _id; set => _id = value; }
+        public decimal Total { get => _total; set => _total = value; }
+        public ObservableCollection<ProdutoModel> Produtos { get => _produtos; set => _produtos = value; }
+    }
+}
---
+++ umfg.venda.app/Models/ProdutoModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace umfg.venda.app.Models
+{
+    public class ProdutoModel
+    {
+        private Guid _id = Guid.NewGuid();
+        private ImageSource _imagem;
+        private string _referencia;
+        private string _descricao;
+        private decimal _valor;
+
+        public Guid Id { get => _id; set => _id = value; }
+        public ImageSource Imagem { get => _imagem; set => _imagem = value; }
+        public string Referencia { get => _referencia ; set => _referencia = value; }
+        public string Descricao { get => _descricao ; set => _descricao = value; }
+        public decimal Valor { get => _valor;   set => _valor = value; }
+    }
+}
